cbd44517f910efc15bdb71b0f314d08eccaa98bf
Samuel Ilesanmi <realsamuel114@gmail.com>
2026-09-07 21:34:53 +0100

    Add rate limiting to transaction routes

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const transactionController = require('../controller/transactionController');
-const webhookController = require('../controller/WebhookController');
 const protect = require('../middleware/auth');
+const { transferLimiter } = require('../middleware/rateLimit');
 
 router.get('/name-enquiry/:accountNumber', protect, transactionController.nameEnquiry);
-router.post('/transfer', protect, transactionController.transferFunds);
-router.post('/buy-airtime', protect, transactionController.buyAirtime);
-router.post('/buy-data', protect, transactionController.buyData);
+router.post('/transfer', protect, transferLimiter, transactionController.transferFunds);
+router.post('/reverse/:transactionId', protect, transactionController.reverseTransaction);
+router.post('/buy-airtime', protect, transferLimiter, transactionController.buyAirtime);
+router.post('/buy-data', protect, transferLimiter, transactionController.buyData);
 router.get('/status/:transactionId', protect, transactionController.getTransactionStatus);
 router.get('/history', protect, transactionController.getUserTransactionHistory);
-
-router.post('/webhook', webhookController.handleNibssWebhook);
 
 module.exports = router;
