df82d8171f77ff055578f91a58afa71ff6804774
Samuel Ilesanmi <realsamuel114@gmail.com>
2026-09-07 21:32:44 +0100

    Add rate limiting to authentication routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,17 @@
-// Src/Routes/AuthRoute.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controller/AuthController');
-const { authLimiter } = require('../middleware/rateLimit');
-const protect  = require('../middleware/auth');
+const { authLimiter, pinLimiter } = require('../middleware/rateLimit');
+const protect = require('../middleware/auth');
 
-
-router.post('/register', authController.registerUser);
+router.post('/register', authLimiter, authController.registerUser);
 router.post('/login', authLimiter, authController.loginUser);
+router.post('/refresh', authController.refreshAccessToken);
 router.post('/send-otp', authLimiter, authController.sendOtp);
 router.post('/resend-otp', authLimiter, authController.resendOTP);
-router.post('/verify-otp', authController.verifyOtp);
+router.post('/verify-otp', authLimiter, authController.verifyOtp);
 router.post('/set-pin', protect, authController.setTransactionPin);
-router.post('/verify-pin', protect, authController.verifyTransactionPin);
-router.post('/logout', authController.logoutUser);
+router.post('/verify-pin', protect, pinLimiter, authController.verifyTransactionPin);
+router.post('/logout', protect, authController.logoutUser);
 
 module.exports = router;
