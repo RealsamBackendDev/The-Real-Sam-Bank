6e28c7b3c4b4e190f5cca652f86f33c62b35ad80
Samuel Ilesanmi <realsamuel114@gmail.com>
2026-09-07 21:42:27 +0100

    Add rate limiting to account creation route

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const accountController = require('../controller/accountController');
 const protect = require('../middleware/auth');
+const { apiLimiter } = require('../middleware/rateLimit');
 
-router.post('/create', protect, accountController.createNewAccount);
+router.post('/create', protect, apiLimiter, accountController.createNewAccount);
 router.get('/all', protect, accountController.getAllAccounts);
 router.get('/balance/:accountNumber', protect, accountController.getAccountBalance);
 router.get('/search/:identifier', protect, accountController.getAccountByIdentifier);
