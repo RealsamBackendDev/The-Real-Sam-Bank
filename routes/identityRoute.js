c0822a17da62d035a255f47e0e98878da796ea35
Samuel Ilesanmi <realsamuel114@gmail.com>
2026-09-07 21:52:15 +0100

    Add rate limiting to Bvn and Nin insertion routes

--- routes/identityRoute.js
+++ routes/identityRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const identityController = require('../controller/IdentityController');
+const identityController = require('../controller/IdentityController'); 
 const protect = require('../middleware/auth');
+const { apiLimiter } = require('../middleware/rateLimit');
 
-router.post('/insertBvn', protect, identityController.createBvn);
-router.post('/insertNin', protect, identityController.createNin);
+
+router.post('/insertBvn', protect, apiLimiter, identityController.createBvn);
+router.post('/insertNin', protect, apiLimiter, identityController.createNin);
 router.post('/validateBvn', protect, identityController.validateBvn);
 router.post('/validateNin', protect, identityController.validateNin);
+
 
 module.exports = router;
